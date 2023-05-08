1db00f8fe791bf4101129341b05c63fbd0e0d8ae
minu-97 <alsdn9891@hanyang.ac.kr>
2023-05-08 09:57:50 +0900

    test

--- model_test/ke-t5_test.py
+++ model_test/ke-t5_test.py
@@ -3,3 +3,4 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 model_name = 'KETI-AIR/ke-t5-base'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
+asdasd
