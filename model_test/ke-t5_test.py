721c85752b8c6dcb51d41b6b2a8c8b6f8e4e7d5c
minu-97 <alsdn9891@hanyang.ac.kr>
2023-05-12 09:49:37 +0900

    ignore

--- model_test/ke-t5_test.py
+++
@@ -1,6 +0,0 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-
-model_name = 'KETI-AIR/ke-t5-base'
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
-asdasd
