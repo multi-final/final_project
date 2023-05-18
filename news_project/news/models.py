1072d26d1a25e5fc0b340641e5adf7a1cf71157c
DevRyu10 <122430128+DevRyu10@users.noreply.github.com>
2023-05-18 09:53:30 +0900

    news 앱 모델 추가
    - 뉴스 기사에 필요한 모델 추가

--- news_project/news/models.py
+++ news_project/news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Press(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32)
+
+
+class Section(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32)
+
+
+class Article(models.Model):
+    id = models.IntegerField(primary_key=True)
+    headline = models.CharField(max_length=128)
+    press = models.ForeignKey(Press, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=32)
+    url = models.CharField(max_length=32)
+    content = models.TextField()
+    created_date = models.DateTimeField()
