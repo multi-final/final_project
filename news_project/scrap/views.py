14e3f49b1c9bb4e94b009aa2c9c10541b7b36986
kihyun <jsh99875@gmail.com>
2023-05-20 14:27:17 +0900

    230520 scroll

--- news_project/scrap/views.py
+++ news_project/scrap/views.py
@@ -34,10 +34,18 @@ def scrap(request):
         else:
             articles = Article.objects.filter(Q(press__in=press_form)&Q(section__in=section_form)).select_related()
         
+        # dbarticles_today = articles.filter(created_date__gte = (datetime.datetime.now()-datetime.timedelta(1)))
+        # dbarticles_yesterday = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(2), datetime.datetime.now()-datetime.timedelta(1)))
+        # dbarticles_week_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
+        # dbarticles_month_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(8)))
         dbarticles_today = articles.filter(created_date__gte = (datetime.datetime.now()-datetime.timedelta(1)))
         dbarticles_yesterday = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(2), datetime.datetime.now()-datetime.timedelta(1)))
-        dbarticles_week_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
-        dbarticles_month_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(8)))
-
-        return render(request, 'scrap/scrap.html', {'dbarticles_today':dbarticles_today, 'dbarticles_yesterday':dbarticles_yesterday, 'dbarticles_this_week':dbarticles_this_week, 'dbarticles_last_week':dbarticles_last_week, 'dbarticles_this_month':dbarticles_this_month, 'dbarticles_long_ago':dbarticles_long_ago})
+        dbarticles_this_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
+        dbarticles_last_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(15), datetime.datetime.now()-datetime.timedelta(8)))
+        dbarticles_this_month = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(15)))
+        dbarticles_long_ago = articles.filter(created_date__lt = (datetime.datetime.now()-datetime.timedelta(31)))
+        context={'dbarticles_today':dbarticles_today, 'dbarticles_yesterday':dbarticles_yesterday,
+                 'dbarticles_this_week':dbarticles_this_week, 'dbarticles_last_week':dbarticles_last_week,
+                 'dbarticles_this_month':dbarticles_this_month, 'dbarticles_long_ago':dbarticles_long_ago}
+        return render(request, 'scrap/scrap.html', context)
         
