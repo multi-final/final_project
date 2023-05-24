f4b41c896a02ec8b095a26e7c028975b6444b950
Eric4848 <hajungsu88@gmail.com>
2023-05-24 17:54:09 +0900

    230524 demo

--- news_project/scrap/views.py
+++ news_project/scrap/views.py
@@ -4,52 +4,41 @@ from scrap.models import Scrap
 from .models import Scrap
 from django.db.models import Q
 from django.http import HttpResponse
-# from .forms import PhotoForm
-# from django.core.files.storage import default_storage
-# from django.core.files.base import ContentFile
-# from django.http import HttpResponse
+
 import datetime
 
 def scrap(request):
     if request.method=='GET':
         now = datetime.datetime.now() # 현재 형식          <= 24시간전 < 데이터 < 48시간전 30
         articles = Scrap.objects.filter(user = request.user).select_related().order_by('-created_date')
-        dbarticles_today = articles.filter(created_date__gte = (datetime.datetime.now()-datetime.timedelta(1)))
-        dbarticles_yesterday = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(2), datetime.datetime.now()-datetime.timedelta(1)))
-        dbarticles_this_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
-        dbarticles_last_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(15), datetime.datetime.now()-datetime.timedelta(8)))
-        dbarticles_this_month = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(15)))
-        dbarticles_long_ago = articles.filter(created_date__lt = (datetime.datetime.now()-datetime.timedelta(31)))
+        dbarticles_today = articles.filter(created_date__gte = (now-datetime.timedelta(1)))
+        dbarticles_this_week = articles.filter(created_date__range = (now-datetime.timedelta(8), now-datetime.timedelta(1)))
+        dbarticles_last_week = articles.filter(created_date__range = (now-datetime.timedelta(15), now-datetime.timedelta(8)))
+        dbarticles_long_ago = articles.filter(created_date__lt = (now-datetime.timedelta(15)))
 
-        return render(request, 'scrap/scrap.html', {'dbarticles_today':dbarticles_today, 'dbarticles_yesterday':dbarticles_yesterday, 'dbarticles_this_week':dbarticles_this_week, 'dbarticles_last_week':dbarticles_last_week, 'dbarticles_this_month':dbarticles_this_month, 'dbarticles_long_ago':dbarticles_long_ago})
+        return render(request, 'scrap/scrap.html', {'dbarticles_today':dbarticles_today, 'dbarticles_this_week':dbarticles_this_week, 'dbarticles_last_week':dbarticles_last_week,'dbarticles_long_ago':dbarticles_long_ago})
     
     else:
         press_form=request.POST.getlist('press')
         section_form=request.POST.getlist('section')
+        user_article = Scrap.objects.filter(user=request.user).select_related().order_by('-created_date')
         if not press_form and not section_form:
-            articles = Article.objects.all().select_related().order_by('-created_date')
+            articles = user_article
         elif press_form and not section_form:
             press_form=list(map(int,press_form))
-            articles = Article.objects.filter(press__in=press_form).select_related()
+            articles = user_article.filter(article__press__in=press_form).select_related()
         elif not press_form and section_form:
             section_form=list(map(int,section_form))
-            articles = Article.objects.filter(section__in=section_form).select_related()
+            articles = user_article.filter(article__section__in=section_form).select_related()
         else:
-            articles = Article.objects.filter(Q(press__in=press_form)&Q(section__in=section_form)).select_related()
-        
-        # dbarticles_today = articles.filter(created_date__gte = (datetime.datetime.now()-datetime.timedelta(1)))
-        # dbarticles_yesterday = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(2), datetime.datetime.now()-datetime.timedelta(1)))
-        # dbarticles_week_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
-        # dbarticles_month_ago = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(8)))
-        dbarticles_today = articles.filter(created_date__gte = (datetime.datetime.now()-datetime.timedelta(1)))
-        dbarticles_yesterday = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(2), datetime.datetime.now()-datetime.timedelta(1)))
-        dbarticles_this_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(8), datetime.datetime.now()-datetime.timedelta(2)))
-        dbarticles_last_week = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(15), datetime.datetime.now()-datetime.timedelta(8)))
-        dbarticles_this_month = articles.filter(created_date__range = (datetime.datetime.now()-datetime.timedelta(31), datetime.datetime.now()-datetime.timedelta(15)))
-        dbarticles_long_ago = articles.filter(created_date__lt = (datetime.datetime.now()-datetime.timedelta(31)))
-        context={'dbarticles_today':dbarticles_today, 'dbarticles_yesterday':dbarticles_yesterday,
-                 'dbarticles_this_week':dbarticles_this_week, 'dbarticles_last_week':dbarticles_last_week,
-                 'dbarticles_this_month':dbarticles_this_month, 'dbarticles_long_ago':dbarticles_long_ago}
+            articles = user_article.filter(Q(article__press__in=press_form)&Q(article__section__in=section_form)).select_related()
+        now = datetime.datetime.now()
+        dbarticles_today = articles.filter(created_date__gte = (now-datetime.timedelta(1)))
+        dbarticles_this_week = articles.filter(created_date__range = (now-datetime.timedelta(8), now-datetime.timedelta(1)))
+        dbarticles_last_week = articles.filter(created_date__range = (now-datetime.timedelta(15), now-datetime.timedelta(8)))
+        dbarticles_long_ago = articles.filter(created_date__lt = (now-datetime.timedelta(15)))
+        context={'dbarticles_today':dbarticles_today,'dbarticles_this_week':dbarticles_this_week,
+                 'dbarticles_last_week':dbarticles_last_week,'dbarticles_long_ago':dbarticles_long_ago}
         return render(request, 'scrap/scrap.html', context)
 
 # class Scrap(models.Model):
