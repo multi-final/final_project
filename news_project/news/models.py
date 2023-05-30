f89ed20bc622ca3825746938b10ae72785d4dd7e
DevRyu10 <122430128+DevRyu10@users.noreply.github.com>
2023-05-30 16:39:17 +0900

    news 모델 변경
    - Press, Section의 id를 int -> char
    - Article 수정
      - id를 django model에서 자동 생성하는 id로 변경
      - writer, url 길이 변경

--- news_project/news/models.py
+++ news_project/news/models.py
@@ -2,21 +2,20 @@ from django.db import models
 
 
 class Press(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=4)
     name = models.CharField(max_length=32)
 
 
 class Section(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=4)
     name = models.CharField(max_length=32)
 
 
 class Article(models.Model):
-    id = models.IntegerField(primary_key=True)
     headline = models.CharField(max_length=128)
     press = models.ForeignKey(Press, on_delete=models.CASCADE)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    writer = models.CharField(max_length=32)
-    url = models.CharField(max_length=32)
+    writer = models.CharField(max_length=128)
+    url = models.CharField(max_length=64)
     content = models.TextField()
     created_date = models.DateTimeField()
