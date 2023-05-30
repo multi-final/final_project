33bae228fcf42556f294f392f0acf60f3b112b3e
Eric4848 <hajungsu88@gmail.com>
2023-05-30 17:49:51 +0900

    230530search02

--- news_project/news/models.py
+++ news_project/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class Press(models.Model):
@@ -20,3 +21,19 @@ class Article(models.Model):
     url = models.CharField(max_length=32)
     content = models.TextField()
     created_date = models.DateTimeField()
+
+    def since_time(self):
+        time_diff = datetime.datetime.now() - self.created_date
+        days = time_diff.days
+        hours, pre_sec = divmod(time_diff.seconds,3600)
+        minutes = pre_sec // 60
+
+        if days >= 1:
+            return f'{days}일 전'
+        elif hours >= 1:
+            return f'{hours}시간 전'
+        elif minutes >= 1:
+            return f'{minutes}분 전'
+        else:
+            return '방금 전'
+        
--- news_project/news/views.py
+++ news_project/news/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Section, Press, Article
+from .models import Article
 from scrap.models import Scrap
-from django.db.models import Q, F
+from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.http import HttpResponse
 import json
+from datetime import datetime as dt
 
 def main(req):
     # 카테고리 미선택 시
@@ -15,6 +15,9 @@ def main(req):
         scrap = {}
         if req.user.is_authenticated:
             scrap = Scrap.objects.filter(user=req.user).select_related().order_by('-article__created_date')
+  
+        # time_now = dt.now()
+        articles_count=articles.count()
         # 페이지네이터로 분리
         paginator = Paginator(articles, 10)
         page = None
@@ -27,9 +30,9 @@ def main(req):
             articles = paginator.page(paginator.num_pages)
         
         if scrap:
-            return render(req, 'news/main.html', {'articles':articles, 'scrap':scrap})
+            return render(req, 'news/main.html', {'articles':articles, 'scrap':scrap , "articles_count":articles_count})
         else:
-            return render(req, 'news/main.html', {'articles':articles})
+            return render(req, 'news/main.html', {'articles':articles, "articles_count":articles_count})
     # 카테고리 선택 시
     else:
         scrap = {}
@@ -62,7 +65,8 @@ def main(req):
                 scrap = scrap.filter(Q(article__press__in=press_form)&Q(article__section__in=section_form)).select_related()
         
         articles = articles.order_by('-created_date')
-        
+        articles_count=articles.count()
+
         paginator = Paginator(articles, 10)
         page = req.POST.get('page')
         try:
@@ -71,18 +75,23 @@ def main(req):
             articles = paginator.page(1)
         except EmptyPage:
             articles = paginator.page(paginator.num_pages)
+
         
         if scrap:
             scrap = scrap.order_by('-article__created_date')
-            return render(req, 'news/main.html', {'articles':articles, "press_list":press_form, "section_list":section_form, "scrap":scrap})
+            return render(req, 'news/main.html', {'articles':articles, "press_list":press_form, "section_list":section_form, "scrap":scrap, "articles_count":articles_count})
         
         else:
-            return render(req, 'news/main.html', {'articles':articles, "press_list":press_form, "section_list":section_form})
+            return render(req, 'news/main.html', {'articles':articles, "press_list":press_form, "section_list":section_form, 'articles_count':articles_count})
 
 def main_ajax(req):
         # 이전에 검색했던 카테고리 정보
-        press_form=list(map(int,req.POST.getlist('press')[0].split()))
-        section_form=list(map(int,req.POST.getlist('section')[0].split()))
+        press_form=0
+        section_form=0
+        if req.POST.get('press') != None:
+            press_form=json.loads(req.POST.get('press'))
+        if req.POST.get('section') != None:
+            section_form=json.loads(req.POST.get('section'))
         
         if not press_form and not section_form:
             post_list = Article.objects.all().select_related()
@@ -100,8 +109,8 @@ def main_ajax(req):
         if req.POST.get('q'):
             search=req.POST.get('q')
             post_list = post_list.filter(Q(headline__contains=search)|Q(content__contains=search)).select_related()
-        
         post_list = post_list.order_by('-created_date')
+
         paginator = Paginator(post_list, 10)
         page = int(req.POST.get('page'))
         try:
@@ -125,27 +134,31 @@ def search(req):
             scrap = Scrap.objects.filter(user=req.user).select_related()
 
         search=req.GET.get('search')
-        press=req.GET.get('press').strip('[]')
-        section=req.GET.get('section').strip('[]')
-        
+
+        if 'press=' in req.GET:
+            press = json.loads(req.GET.get('press'))
+        else:
+            press=None
+
+        if 'section=' in req.GET:
+            section = json.loads(req.GET.get('section'))
+        else:
+            section=None
+
         if not press and not section:
             post_list = Article.objects.all().select_related()
             
         elif press and not section:
-            press=list(map(int,press.split(',')))
             post_list = Article.objects.filter(press__in=press).select_related()
             if scrap:
                 scrap = scrap.filter(article__press__in=press).select_related()
 
         elif not press and section:
-            section=list(map(int,section.split(',')))
             post_list = Article.objects.filter(section__in=section).select_related()
             if scrap:
                 scrap = scrap.filter(article__section__in=section).select_related()
 
         else:
-            press=list(map(int,press.split(',')))
-            section=list(map(int,section.split(',')))
             post_list = Article.objects.filter(Q(press__in=press)&Q(section__in=section)).select_related()
             if scrap:
                 scrap = scrap.filter(Q(article__press__in=press)&Q(article__section__in=section)).select_related()
@@ -153,6 +166,7 @@ def search(req):
         post_list = post_list.filter(Q(headline__contains=search)|Q(content__contains=search)).select_related()
         
         post_list = post_list.order_by('-created_date')
+        articles_count = post_list.count()
         paginator = Paginator(post_list, 10)
         page = req.POST.get('page')
         try:
@@ -164,19 +178,15 @@ def search(req):
 
         if scrap:
             scrap = scrap.order_by('-article__created_date')
-            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "scrap":scrap, "q":search})
+            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "scrap":scrap, "q":search, "articles_count":articles_count})
             
         else:
-            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "q":search})
+            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "q":search, "articles_count":articles_count})
     else:
         scrap={}
         if req.user.is_authenticated:
             scrap = Scrap.objects.filter(user=req.user).select_related()
         search=req.GET.get('search')
-        if 'press' in req.GET:
-            del req.GET['press']
-        if 'section' in req.GET:
-            del req.GET['section']
         press=req.POST.getlist('press')
         section=req.POST.getlist('section')
         if not press and not section:
@@ -211,8 +221,7 @@ def search(req):
 
         if scrap:
             scrap = scrap.order_by('-article__created_date')
-            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "scrap":scrap, "q":search})
+            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "scrap":scrap, "q":search, 'articles_count':articles_count})
             
         else:
-            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "q":search})
-    
+            return render(req, 'news/main.html', {'articles':post_list, "press_list":press, "section_list":section, "q":search, 'articles_count':articles_count})
