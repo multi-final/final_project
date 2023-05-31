6e0ebf6b179fc8597ef478926d9f300c308287fa
kihyun <jsh99875@gmail.com>
2023-05-31 17:57:43 +0900

    230531

--- news_project/news/urls.py
+++ news_project/news/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'news'
 urlpatterns = [
     path('', views.main, name = 'main'),
+    path('main/',views.index, name = 'index'),
     path('main_ajax/', views.main_ajax, name='main_ajax'),
     path('search/', views.search, name='search'),
 ]
