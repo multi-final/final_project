994338d541d99b5e2ce34ee8aacfb0fd3a34ca72
Eric4848 <hajungsu88@gmail.com>
2023-06-10 11:12:59 +0900

    230610main

--- news_project/news/urls.py
+++ news_project/news/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'news'
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('main/', views.main, name = 'main'),
+    path('', views.main, name = 'main'),
+    path('index/',views.index, name = 'index'),
     path('main_ajax/', views.main_ajax, name='main_ajax'),
     path('search/', views.search, name='search'),
     path('introduction/', views.intro, name='intro'),
